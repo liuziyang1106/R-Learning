e26e4c0ce7e0422276d3931771be11e8f743a324
liuziyang <liuziyang1106@sina.com>
2022-02-10 09:47:32 +0800

    array

--- 2-Data_Set/2-1-R-Data-Struture/2-1-4-Array_and_List.R
+++ 2-Data_Set/2-1-R-Data-Struture/2-1-4-Array_and_List.R
@@ -14,3 +14,16 @@ print(A1)
 
 
 # List
+# 列表是R中最灵活，也是最复杂的数据结构。它可以是多种类型对象的组合
+list1 <- list(a = 1, b = 1:5, c = c("red", "blue"))
+print(list1)
+
+
+# 在普通的数据分析中，创建列表并不是一项常见的任务。但是很多函数的返回值是一个列表
+set.seed(123)
+dat <- rnorm(10)
+bp <- boxplot(dat)
+print(class(bp))
+
+# 这里列表bp包含了很多对象，如果想查看或者使用某个对象，使用$ 引用
+print(bp$stats)
